e7c0ce5d3a0edd702df3f380fca703324213007c
Manaheeljamil <manaheeljamil20@gmail.com>
2022-06-02 07:44:11 -0700

    First commit

---
+++ note.js
@@ -0,0 +1,21 @@
+
+// Question # 3:
+// function remove(e){
+//     e.parentNode.parentNode.remove()
+
+// }
+// Question # 5:
+// function increase() {
+//     var value = document.getElementById("counter").innerHTML
+//     value = parseInt(value)
+//     console.log(value)
+//     value = value + 1
+//     document.getElementById("counter").innerHTML = value
+// }
+//  function decrease() {
+//     var value = document.getElementById("counter").innerHTML
+//     value = parseInt(value)
+//     value = value - 1
+//     document.getElementById("counter").innerHTML = value
+// }
+    
